7a164c8ae2dfe89bc4cbd2fece19bab608691145
kdy071115 <kdy071115@naver.com>
2024-07-13 01:47:08 +0900

    new

--- src/main/java/com/kody/kiwi/domain/request/CheckRequest.java
+++ src/main/java/com/kody/kiwi/domain/request/CheckRequest.java
@@ -1,9 +1,6 @@
 package com.kody.kiwi.domain.request;
 
-import com.kody.kiwi.domain.entity.enums.Gender;
-import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Data
